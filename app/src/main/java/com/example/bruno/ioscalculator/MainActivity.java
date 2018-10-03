f75b9da4d27cc1ac59408db6b9610abbc7eba3a5
Bruno Simione Beltrame <bruno.simione@gmail.com>
2018-10-03 01:06:18 -0400

    Initial commit

---
+++ app/src/main/java/com/example/bruno/ioscalculator/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.bruno.ioscalculator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final TextView txt1 = (TextView) findViewById(R.id.numberText);
+
+        Button b1 = (Button) findViewById(R.id.btn1);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                txt1.append("1");
+            }
+        });
+
+    }
+}
